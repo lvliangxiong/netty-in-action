29e726e32218742ff839b8b493600eeffd8b86d7
joeylyu <lvliangxiong@foxmail.com>
2020-10-13 09:19:51 +0800

    add comments in chapter 1

--- chapter1/src/main/java/nia/chapter1/BlockingIoExample.java
+++ chapter1/src/main/java/nia/chapter1/BlockingIoExample.java
@@ -9,23 +9,29 @@ import java.net.Socket;
 
 /**
  * Created by kerr.
- *
+ * <p>
  * Listing 1.1 Blocking I/O example
  */
 public class BlockingIoExample {
 
     /**
      * Listing 1.1 Blocking I/O example
-     * */
+     */
     public void serve(int portNumber) throws IOException {
+        // 创建一个绑定指定端口的 ServerSocket 对象
         ServerSocket serverSocket = new ServerSocket(portNumber);
+        // ServerSocket 对象阻塞式监听请求，直到有连接请求输入
+        // 此时 ServerSocket 将为发出连接请求的 Socket 创建一个专门的 Socket 对象用于消息通信，并作为 accept 方法的返回值
         Socket clientSocket = serverSocket.accept();
+
+        // 通过 Socket 对象获取输入输出流进行消息通信
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(clientSocket.getInputStream()));
         PrintWriter out =
                 new PrintWriter(clientSocket.getOutputStream(), true);
+
         String request, response;
-        while ((request = in.readLine()) != null) {
+        while ((request = in.readLine()) != null/*阻塞式获取客户端发送的消息*/) {
             if ("Done".equals(request)) {
                 break;
             }
@@ -34,7 +40,7 @@ public class BlockingIoExample {
         }
     }
 
-    private String processRequest(String request){
+    private String processRequest(String request) {
         return "Processed";
     }
 }
--- chapter1/src/main/java/nia/chapter1/ConnectExample.java
+++ chapter1/src/main/java/nia/chapter1/ConnectExample.java
@@ -12,9 +12,9 @@ import java.nio.charset.Charset;
 
 /**
  * Created by kerr.
- *
+ * <p>
  * Listing 1.3 Asynchronous connect
- *
+ * <p>
  * Listing 1.4 Callback in action
  */
 public class ConnectExample {
@@ -22,15 +22,21 @@ public class ConnectExample {
 
     /**
      * Listing 1.3 Asynchronous connect
-     *
+     * <p>
      * Listing 1.4 Callback in action
-     * */
+     */
     public static void connect() {
         Channel channel = CHANNEL_FROM_SOMEWHERE; //reference form somewhere
         // Does not block
         ChannelFuture future = channel.connect(
                 new InetSocketAddress("192.168.0.1", 25));
+
+        // ChannelFuture 对象可以看做一个异步操作的结果的占位符
         future.addListener(new ChannelFutureListener() {
+            /**
+             * 此方法将在 future 对应的异步操作已经完成时，被回调
+             * @param future
+             */
             @Override
             public void operationComplete(ChannelFuture future) {
                 if (future.isSuccess()) {
